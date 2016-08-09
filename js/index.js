e1ab19d8026925e37dc7ff2c4319a47b28f515f7
Billy Cook <wcook@crec.net>
2016-08-09 17:50:39 -0400

    Added Firebase auth and post functionality

--- js/index.js
+++ js/index.js
@@ -40,14 +40,22 @@ var monthNames = [
 
 Handlebars.registerHelper('formatDate', function(date) {
   var date = new Date(date);
-  date = monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+  date = monthNames[date.getUTCMonth()] + ' ' + date.getUTCDate() + ', ' + date.getUTCFullYear();
 
   return date;
 });
 
 function compileTemplates() {
+    loginTemplate = $('#login-template').html();
+    loginTemplate = Handlebars.compile(loginTemplate);
+
     jobsListTemplate = $('#jobs-list-template').html();
     jobsListTemplate = Handlebars.compile(jobsListTemplate);
+}
+
+function renderLogin() {
+	var loginHtml = loginTemplate(model);
+	$('#loginForm').html(loginHtml);
 }
 
 function renderJobsList () {
@@ -63,19 +71,87 @@ function renderNewJob () {
     $('#jobsList').prepend(jobsListHTML);
 }
 
+function processJobs (snapshot){
+	console.log('db update callback');
+	model.jobs = snapshot.val();
+	renderJobsList();
+}
+
 
 
 // Controller
 
 function setup() {
     compileTemplates();
+    renderLogin();
     renderJobsList();
 
-    $('#jobForm').on('click', '#addJob', addJob);
+    //Auth
+	$('#loginForm').on('click', '#register', handleRegistration);
+	$('#loginForm').on('click', '#login', handleLogin);
+	$('#loginForm').on('click', '#signOut', handleSignOut);
+	firebase.auth().onAuthStateChanged(handleAuthStateChange);
+
+
+    $('#jobForm').on('click', '#addJob', handleAddJob);
+    firebase.database().ref('jobs').on('value', processJobs);
 }
 
-function addJob () {
-    model.jobs.push({
+function handleRegistration() {
+	console.log('register');
+	var email = $('input[name="email"]').val();
+	var password = $('input[name="password"]').val();
+
+	firebase.auth().createUserWithEmailAndPassword(email, password);
+}
+
+function handleLogin() {
+	console.log('login');
+	var email = $('input[name="email"]').val();
+	var password = $('input[name="password"]').val();
+
+	firebase.auth().signInWithEmailAndPassword(email, password);
+}
+
+function handleSignOut() {
+	console.log('sign out');
+	firebase.auth().signOut();
+}
+
+function handleAuthStateChange() {
+	console.log('auth state change');
+	var user = firebase.auth().currentUser;
+
+	if (user) {
+		model.loggedIn = true;
+		model.user = user;
+
+		//firebase.database().ref('jobs').on('value', processMessages);
+	} else {
+		model.loggedIn = false;
+		model.user = user;
+	}
+	renderLogin();
+	//renderChat();
+}
+
+
+function handleAddJob () {
+    /*model.jobs.push({
+        guid:3,
+        title: $('input[name="title"]').val(),
+        organization: $('input[name="organization"]').val(),
+        location: $('input[name="location"]').val(),
+        description: $('textarea[name="description"]').val(),
+        pubDate: Date.parse($('input[name="datePosted"]').val()),
+        expirationDate: Date.parse($('input[name="dateExpires"]').val()),
+        filledDate:'',
+        source:'/',
+        sourceText:'More Info'
+    })*/
+	console.log('add job');
+	//if (message){
+    firebase.database().ref('jobs').push({
         guid:3,
         title: $('input[name="title"]').val(),
         organization: $('input[name="organization"]').val(),
@@ -86,9 +162,9 @@ function addJob () {
         filledDate:'',
         source:'/',
         sourceText:'More Info'
-    })
-
-    renderJobsList();
+		});
+	//}
+    //renderJobsList();
 }
 
 
